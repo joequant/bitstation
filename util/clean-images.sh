942626ee726c48b4a92c8ec65f7f65580f13dba8
Joseph C Wang <joequant@gmail.com>
2022-04-18 05:00:47 +0800

    fix script to put in script dir

--- util/clean-images.sh
+++ util/clean-images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for i in 'podman' 'buildah' 'docker'  ; do
-    ./rm-stopped-containers.sh $i
-    ./rm-untagged-images.sh $i
+    $script_dir/rm-stopped-containers.sh $i
+    $script_dir/rm-untagged-images.sh $i
 done
